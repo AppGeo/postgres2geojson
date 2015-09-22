ecbf11e862a07a9ec50ca7bae9a1e7991c963173
Calvin Metcalf <cmetcalf@appgeo.com>
2015-09-22 12:20:08 -0400

    make it a test closeness

--- test.js
+++ test.js
@@ -12,7 +12,9 @@ test('compare', function (t) {
   }).on('end', function () {
     t.equals(data.length, foo.length, 'correct length');
     data.forEach(function (item, i) {
-      t.deepEquals(item, foo[i], 'check item ' + i);
+      t.ok(Math.abs(item.geometry.coordinates[0] - foo[i].geometry.coordinates[0]) < Number.EPSILON, 'equal longatudes');
+      t.ok(Math.abs(item.geometry.coordinates[1] - foo[i].geometry.coordinates[1]) < Number.EPSILON, 'equal latatudes');
+      t.deepEquals(item.properties, foo[i].properties, 'check properties ' + i);
     });
     t.end();
   });
